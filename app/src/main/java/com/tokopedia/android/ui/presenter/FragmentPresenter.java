e610172f66f42c3363c79b24317618587531e423
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-16 12:55:17 +0700

    add presenter

---
+++ app/src/main/java/com/tokopedia/android/ui/presenter/FragmentPresenter.java
@@ -0,0 +1,11 @@
+package com.tokopedia.android.ui.presenter;
+
+/**
+ * Created by raditya.gumay on 16/02/2016.
+ */
+public interface FragmentPresenter {
+    public void onResume();
+    public void onDestroy();
+    public void onStop();
+    public void onGetProduct();
+}
---
+++ app/src/main/java/com/tokopedia/android/ui/view/ProductView.java
@@ -0,0 +1,13 @@
+package com.tokopedia.android.ui.view;
+
+import java.util.List;
+
+/**
+ * Created by raditya.gumay on 16/02/2016.
+ */
+public interface ProductView {
+    public void showProgress();
+    public void hideProgress();
+    public void setItems(List<String> items);
+    public void showMessage(String message);
+}
