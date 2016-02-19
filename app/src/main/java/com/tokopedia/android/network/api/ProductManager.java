950f1b049285351527cadc33fafcc8246b50c1a6
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-19 13:24:36 +0700

    add config

--- app/src/main/java/com/tokopedia/android/network/api/ProductManager.java
+++ app/src/main/java/com/tokopedia/android/network/api/ProductManager.java
@@ -1,17 +1,10 @@
 package com.tokopedia.android.network.api;
 
-import com.google.common.collect.ImmutableList;
 import com.tokopedia.android.network.RestService;
-import com.tokopedia.android.service.Repository;
-import com.tokopedia.android.service.User;
 import com.tokopedia.android.service.response.ProductResponse;
-import com.tokopedia.android.service.response.RepositoryResponse;
-
-import java.util.List;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -25,28 +18,12 @@ public class ProductManager {
         this.restService = restService;
     }
 
-    public Observable<ProductResponse> getProductApis() {
+    /**
+     * Simple get
+     * @return
+     */
+    public Observable<ProductResponse> getProductRepository() {
         return restService.getProducts()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-    }
-
-    public Observable<ImmutableList<Repository>> getUsersRepositories() {
-        return restService.getUsersRepositories("jack")
-                .map(new Func1<List<RepositoryResponse>, ImmutableList<Repository>>() {
-                    @Override
-                    public ImmutableList<Repository> call(List<RepositoryResponse> repositoriesListResponse) {
-                        final ImmutableList.Builder<Repository> listBuilder = ImmutableList.builder();
-                        for (RepositoryResponse repositoryResponse : repositoriesListResponse) {
-                            Repository repository = new Repository();
-                            repository.id = repositoryResponse.id;
-                            repository.name = repositoryResponse.name;
-                            repository.url = repositoryResponse.url;
-                            listBuilder.add(repository);
-                        }
-                        return listBuilder.build();
-                    }
-                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
