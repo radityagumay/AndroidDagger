edc5a7cb078f1e7797bbb21aed6fa5d7669a017b
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-19 13:24:37 +0700

    add config

--- app/src/main/java/com/tokopedia/android/ui/base/BaseActivity.java
+++ app/src/main/java/com/tokopedia/android/ui/base/BaseActivity.java
@@ -1,13 +1,10 @@
 package com.tokopedia.android.ui.base;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tokopedia.android.TokopediaApplication;
 import com.tokopedia.android.injection.component.ApplicationComponent;
-import com.tokopedia.android.injection.component.ProductComponent;
-import com.tokopedia.android.injection.component.UserComponent;
 
 /**
  * Created by raditya.gumay on 16/02/2016.
@@ -17,13 +14,14 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initInjection();
+    }
+
+    private void initInjection(){
+        applicationComponent().inject(this);
     }
 
     public ApplicationComponent applicationComponent() {
         return TokopediaApplication.get(this).getComponent();
     }
-
-    /*public UserComponent userComponent() {
-        return TokopediaApplication.get(this).getUserComponent();
-    }*/
 }
