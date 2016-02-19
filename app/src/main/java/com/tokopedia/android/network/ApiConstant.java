900086c547c5405dbd97dab758d8e90505d2e60b
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-19 13:24:36 +0700

    add config

--- app/src/main/java/com/tokopedia/android/network/ApiConstant.java
+++ app/src/main/java/com/tokopedia/android/network/ApiConstant.java
@@ -5,14 +5,10 @@ package com.tokopedia.android.network;
  */
 public class ApiConstant {
 
-    public static final String CONTENT_TYPE_JSON = "Content-Type: application/json";
-
     public static final long HTTP_CONNECT_TIMEOUT = 60 * 1000;
     public static final long HTTP_READ_TIMEOUT = 60 * 1000;
 
     public static final String BASE_URL = "http://ajax.tokopedia.com/search/v1/";
-    //public static final String BASE_URL = "http://swapi.co/api/";
-    //public static final String BASE_URL = "https://api.github.com";
 
     /**
      * your path
