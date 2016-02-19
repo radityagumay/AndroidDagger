e087f44abe4a6f1f7833dbb5f996192224f9dc53
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-19 13:24:36 +0700

    add config

--- app/src/main/java/com/tokopedia/android/network/RestService.java
+++ app/src/main/java/com/tokopedia/android/network/RestService.java
@@ -1,14 +1,8 @@
 package com.tokopedia.android.network;
 
-import com.tokopedia.android.service.response.BaseResponse;
 import com.tokopedia.android.service.response.ProductResponse;
-import com.tokopedia.android.service.response.RepositoryResponse;
-import com.tokopedia.android.service.response.UserResponse;
-
-import java.util.List;
 
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import rx.Observable;
 
 /**
@@ -19,18 +13,5 @@ public interface RestService {
     @GET(ApiConstant.PRODUCT)
     Observable<ProductResponse> getProducts(
             // TODO PATH
-    );
-
-    @GET("/people/{personId}")
-    Observable<com.tokopedia.android.service.Character> getCharacter(@Path("personId") int id);
-
-    @GET("/users/{username}/repos")
-    Observable<List<RepositoryResponse>> getUsersRepositories(
-            @Path("username") String username
-    );
-
-    @GET("/users/{username}")
-    Observable<UserResponse> getUser(
-            @Path("username") String username
     );
 }
