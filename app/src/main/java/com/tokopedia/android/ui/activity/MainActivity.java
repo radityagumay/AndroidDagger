645c50031da41446caf661b304751cbc873d7f92
R Aditya Gumay <gumay.raditya@yahoo.com>
2016-02-19 13:24:37 +0700

    add config

--- app/src/main/java/com/tokopedia/android/ui/activity/MainActivity.java
+++ app/src/main/java/com/tokopedia/android/ui/activity/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        applicationComponent().inject(this);
 
         setContentView(R.layout.activity_main);
         initInjectView();
